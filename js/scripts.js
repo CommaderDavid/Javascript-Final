26aee753f49347e2e1fa49b2e00e9e4131da059f
David Boedigheimer <stealth259@gmail.com>
2019-10-15 13:47:17 -0700

    Add backend function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // Backend
-
+var whichTra = function(work, style, fun, design, back) {
+  if (work === "large" && style === "c" && back === "c") {
+    return "C#/.Net"
+  } else if (work === "droid" && style === "java" && fun === "game" && design === "css") {
+    return "JavaScript"
+  } else if (work === "small" && style === "php" && fun === "soc") {
+    return "PHP"
+  } else if (work === "start" && style === "ruby" && fun === "prob" && back === "ruby") {
+    return "Ruby/Rails"
+  } else if (fun === "game" && design === "css" && back === "css") {
+    return "CSS/React"
+  }
+}
 
 // Frontend
 $(document).ready(function() {
@@ -9,6 +21,8 @@ $(document).ready(function() {
     var fun = $("select#fun").val();
     var design = $("select#design").val();
     var back = $("select#back").val();
+
+    var result =
 
     e.preventDefault();
   });
